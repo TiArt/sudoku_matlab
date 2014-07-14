c722890ccd7279ca5fa96821f8987b060cfede46
Burak Aktas <ckaktus@cyber-kaktus.de>
2014-07-14 12:02:22 +0200

    kand.m fehlerbereinigt

--- kand.m
+++ kand.m
@@ -1,8 +1,8 @@
-function [] = kand(A) 
+function [] = kand(A,n) 
  
 for I = 1:n^2
     if A(I)==0
-	spalte=idivide (I n, "floor")
+	spalte=idivide (int8(I), int8(n), 'floor')
 	spaltens=spalte*n+1
            spaltene=spaltens+n-1
            
@@ -11,29 +11,29 @@ for I = 1:n^2
            
 	if I==mod(I,sqrt(n))
 		if mod(I,sqrt(n)) == 0
-		      sub=zero(1:sqrt(n)-1)
+		      sub=zeros(1:sqrt(n)-1)
 		      for j=1:sqrt(n)-1
 			  sub(j)=A(I+j*n-2:I+j*n-1)
 		      end
 		elseif mod(I,sqrt(n)) == sqrt(n)-1
-		      sub=zero(1:sqrt(n)-1)
+		      sub=zeros(1:sqrt(n)-1)
 		      for j=1:sqrt(n)-1
 			  sub(j)=A(I-j*n-2:I-j*n-1)
 		      end
 		else
-			sub=zero(1:sqrt(n)-1)
+			sub=zeros(1:sqrt(n)-1)
+			I=zeilens
 			for j=0:sqrt(n)-2
 				if j==spalte
 					j=j+1
 				end
-				I=zeilens
-				sub(j)=A(I-j*n-2:I-j*n-1)
+				sub(j+1)=A(I+j*n-2:I+j*n-1)
 			end
 		end
 	end
 		      
       
-           A=sort(unique(cat(2, A(zeilens:9:81), A(spaltens:1:spaltene), sub(1),sub(2),sub(3))
+           A=sort(unique(cat(2, A(zeilens:9:81), A(spaltens:1:spaltene), sub(1),sub(2),sub(3))))
            %A=sort(unique(cat(2, A(zeilens:9:81), A(spaltens:1:spaltene), A(1:3), A(10:12), A(19:21))))
     end
 end
